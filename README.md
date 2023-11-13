7d36494ee41b8fdb5b9095a2e31a8db4ffa07b3b
crazyman2133 <148265659+crazyman2133@users.noreply.github.com>
2023-11-13 08:13:06 -0500

    Added enemy spawn and timer logic

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Dodge the Creeps
 
-This gane follows "Your first 2D game" tutorial:
+This game follows "Your first 2D game" tutorial:
 
 https://docs.godotengine.org/en/latest/getting_started/first_2d_game/index.html
--- player.gd
+++ player.gd
@@ -1,16 +1,15 @@
 extends Area2D
 signal hit
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size
+@export var speed = 400 #Player speed in pixles per second
+var screen_size 
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	if velocity.x != 0:
 	hide()
 
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	var velocity = Vector2.ZERO # The player's movement vector
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -19,7 +18,7 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-
+		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
@@ -30,7 +29,6 @@ func _process(delta):
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
-		# See the note below about boolean assignment.
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
@@ -38,4 +36,11 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	pass # Replace with function body.
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
