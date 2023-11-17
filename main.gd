d83e12705bf2a9b6ec439f3cfe3ed572dedac314
crazyman2133 <148265659+crazyman2133@users.noreply.github.com>
2023-11-17 13:26:05 -0500

    Added the main licenses for Godot

--- main.gd
+++ main.gd
@@ -9,11 +9,18 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
+	get_tree().call_group("mobs", "queue_free")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_mob_timer_timeout():
 	# Create a new instanece of the Mob scene
@@ -42,6 +49,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
